fba2a58d4c62a994c3c985446441dda58e638335
lopez <patrice.lopez@science-miner.com>
2019-09-16 21:59:13 +0200

    add n-fold train eval

--- src/main/java/org/grobid/trainer/SoftwareTrainerRunner.java
+++ src/main/java/org/grobid/trainer/SoftwareTrainerRunner.java
@@ -13,13 +13,13 @@ import java.util.Arrays;
  */
 public class SoftwareTrainerRunner {
 
-    private static final String USAGE = "Usage: {0 - train, 1 - evaluate, 2 - split, train and evaluate} {software} "
+    private static final String USAGE = "Usage: {0 - train, 1 - evaluate, 2 - split, train and evaluate, 3 - eval with n-fold} {software} "
             + "-s { [0.0 - 1.0] - split ratio, optional} "
             + "-b {epsilon, window, nbMax}"
             + "-t NBThreads";
 
     enum RunType {
-        TRAIN, EVAL, SPLIT;
+        TRAIN, EVAL, SPLIT, EVAL_N_FOLD;
 
         public static RunType getRunType(int i) {
             for (RunType t : values()) {
@@ -81,6 +81,7 @@ public class SoftwareTrainerRunner {
         double epsilon = 0.000001;
         int window = 20;
         int nbMaxIterations = 0;
+        int numFolds = 10;
 
         for (int i = 0; i < args.length; i++) {
             if (i == 4) {
@@ -92,7 +93,17 @@ public class SoftwareTrainerRunner {
                 } catch (Exception e) {
                     throw new IllegalStateException("Invalid split value: " + args[i]);
                 }
-            } /*else if (args[i].equals("-b")) {
+            } else if (args[i].equals("-n")) {
+                if (i + 1 == args.length) {
+                    throw new IllegalStateException("Missing number of folds value. ");
+                }
+                try {
+                    numFolds = Integer.parseInt(args[i + 1]);
+                } catch (Exception e) {
+                    throw new IllegalStateException("Invalid number of folds value: " + args[i + 1]);
+                }
+            }
+            /*else if (args[i].equals("-b")) {
                 if ((mode == RunType.TRAIN) && (args.length >= 7)) {
                     breakParams = true;
                     epsilon = Double.parseDouble(args[i + 1]);
@@ -121,6 +132,21 @@ public class SoftwareTrainerRunner {
                 break;
             case SPLIT:
                 System.out.println(AbstractTrainer.runSplitTrainingEvaluation(trainer, split));
+                break;
+            case EVAL_N_FOLD:
+                if(numFolds == 0) {
+                    throw new IllegalArgumentException("N should be > 0");
+                }
+                /*if (StringUtils.isNotEmpty(outputFilePath)) {
+                    Path outputPath = Paths.get(outputFilePath);
+                    if (Files.exists(outputPath)) {
+                        System.err.println("Output file exists. ");
+                    }
+                } else */ 
+                {
+                    String results = AbstractTrainer.runNFoldEvaluation(trainer, numFolds);
+                    System.out.println(results);
+                }
                 break;
             default:
                 throw new IllegalStateException("Invalid RunType: " + mode.name());
