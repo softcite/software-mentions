fe6db29bd23a392319de65b79d0f24e10dc088ee
lopez <patrice.lopez@science-miner.com>
2023-02-21 15:18:19 +0100

    review config

--- src/main/java/org/grobid/service/configuration/SoftwareServiceConfiguration.java
+++ src/main/java/org/grobid/service/configuration/SoftwareServiceConfiguration.java
@@ -2,11 +2,20 @@ package org.grobid.service.configuration;
 
 import io.dropwizard.Configuration;
 import org.grobid.core.utilities.SoftwareConfiguration;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class SoftwareServiceConfiguration extends Configuration {
 
     private String grobidHome;
     private SoftwareConfiguration softwareConfiguration;
+    private int maxParallelRequests;
+
+    @JsonProperty
+    private String corsAllowedOrigins = "*";
+    @JsonProperty
+    private String corsAllowedMethods = "OPTIONS,GET,PUT,POST,DELETE,HEAD";
+    @JsonProperty
+    private String corsAllowedHeaders = "X-Requested-With,Content-Type,Accept,Origin";
 
     public String getGrobidHome() {
         return grobidHome;
@@ -22,5 +31,36 @@ public class SoftwareServiceConfiguration extends Configuration {
 
     public void setSoftwareConfiguration(SoftwareConfiguration conf) {
         this.softwareConfiguration = conf;
+    }
+
+    public int getMaxParallelRequests() {
+        if (this.maxParallelRequests == 0) {
+            this.maxParallelRequests = Runtime.getRuntime().availableProcessors();
+        }
+        return this.maxParallelRequests;
+    }
+
+    public String getCorsAllowedOrigins() {
+        return corsAllowedOrigins;
+    }
+
+    public void setCorsAllowedOrigins(String corsAllowedOrigins) {
+        this.corsAllowedOrigins = corsAllowedOrigins;
+    }
+
+    public String getCorsAllowedMethods() {
+        return corsAllowedMethods;
+    }
+
+    public void setCorsAllowedMethods(String corsAllowedMethods) {
+        this.corsAllowedMethods = corsAllowedMethods;
+    }
+
+    public String getCorsAllowedHeaders() {
+        return corsAllowedHeaders;
+    }
+
+    public void setCorsAllowedHeaders(String corsAllowedHeaders) {
+        this.corsAllowedHeaders = corsAllowedHeaders;
     }
 }
