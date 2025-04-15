ce8b98ca5f2810d78644c062236f71600aadfefd
Luca Foppiano <luca@foppiano.org>
2025-04-15 14:18:33 +0200

    update to dropwizard 4.x

--- src/main/java/org/grobid/service/SoftwareServiceModule.java
+++ src/main/java/org/grobid/service/SoftwareServiceModule.java
@@ -1,49 +1,31 @@
 package org.grobid.service;
 
-import com.codahale.metrics.MetricRegistry;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.inject.Binder;
 import com.google.inject.Provides;
-import com.hubspot.dropwizard.guicier.DropwizardAwareModule;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
 import org.grobid.service.configuration.SoftwareServiceConfiguration;
-import org.grobid.service.controller.SoftwareController;
 import org.grobid.service.controller.HealthCheck;
+import org.grobid.service.controller.SoftwareController;
 import org.grobid.service.controller.SoftwareProcessFile;
 import org.grobid.service.controller.SoftwareProcessString;
+import ru.vyarus.dropwizard.guice.module.support.DropwizardAwareModule;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 
 
 public class SoftwareServiceModule extends DropwizardAwareModule<SoftwareServiceConfiguration> {
 
     @Override
-    public void configure(Binder binder) {
+    public void configure() {
         // Generic modules
-        binder.bind(GrobidEngineInitialiser.class);
-        binder.bind(HealthCheck.class);
+        bind(GrobidEngineInitialiser.class);
+        bind(HealthCheck.class);
 
         // Core components
-        binder.bind(SoftwareProcessFile.class);
-        binder.bind(SoftwareProcessString.class);
+        bind(SoftwareProcessFile.class);
+        bind(SoftwareProcessString.class);
 
         // REST
-        binder.bind(SoftwareController.class);
-    }
-
-    @Provides
-    protected ObjectMapper getObjectMapper() {
-        return getEnvironment().getObjectMapper();
-    }
-
-    @Provides
-    protected MetricRegistry provideMetricRegistry() {
-        return getMetricRegistry();
-    }
-
-    //for unit tests
-    protected MetricRegistry getMetricRegistry() {
-        return getEnvironment().metrics();
+        bind(SoftwareController.class);
     }
 
     @Provides
--- src/main/java/org/grobid/service/configuration/SoftwareServiceConfiguration.java
+++ src/main/java/org/grobid/service/configuration/SoftwareServiceConfiguration.java
@@ -1,19 +1,37 @@
 package org.grobid.service.configuration;
 
-import io.dropwizard.Configuration;
+import io.dropwizard.core.Configuration;
+import org.grobid.core.utilities.GrobidConfig;
 import org.grobid.core.utilities.SoftwareConfiguration;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SoftwareServiceConfiguration extends Configuration {
 
     private String grobidHome;
     private SoftwareConfiguration softwareConfiguration;
     private int maxParallelRequests;
+    private String version;
+
+    private String entityFishingHost;
+    private String entityFishingPort;
+
+    private String corpusPath;
+    private String tmpPath;
+
+    private String pub2teiPath;
+    private Boolean useBinaryContextClassifiers;
+
+    private List<GrobidConfig.ModelParameters> models = new ArrayList<>();
 
     @JsonProperty
     private String corsAllowedOrigins = "*";
+
     @JsonProperty
     private String corsAllowedMethods = "OPTIONS,GET,PUT,POST,DELETE,HEAD";
+
     @JsonProperty
     private String corsAllowedHeaders = "X-Requested-With,Content-Type,Accept,Origin";
 
@@ -62,5 +80,65 @@ public class SoftwareServiceConfiguration extends Configuration {
 
     public void setCorsAllowedHeaders(String corsAllowedHeaders) {
         this.corsAllowedHeaders = corsAllowedHeaders;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getEntityFishingHost() {
+        return entityFishingHost;
+    }
+
+    public void setEntityFishingHost(String entityFishingHost) {
+        this.entityFishingHost = entityFishingHost;
+    }
+
+    public String getEntityFishingPort() {
+        return entityFishingPort;
+    }
+
+    public void setEntityFishingPort(String entityFishingPort) {
+        this.entityFishingPort = entityFishingPort;
+    }
+
+    public String getCorpusPath() {
+        return corpusPath;
+    }
+
+    public void setCorpusPath(String corpusPath) {
+        this.corpusPath = corpusPath;
+    }
+
+    public String getTmpPath() {
+        return tmpPath;
+    }
+
+    public void setTmpPath(String tmpPath) {
+        this.tmpPath = tmpPath;
+    }
+
+    public String getPub2teiPath() {
+        return pub2teiPath;
+    }
+
+    public void setPub2teiPath(String pub2teiPath) {
+        this.pub2teiPath = pub2teiPath;
+    }
+
+    public Boolean getUseBinaryContextClassifiers() {
+        return useBinaryContextClassifiers;
+    }
+
+    public void setUseBinaryContextClassifiers(Boolean useBinaryContextClassifiers) {
+        this.useBinaryContextClassifiers = useBinaryContextClassifiers;
+    }
+
+    public List<GrobidConfig.ModelParameters> getModels() {
+        return models;
+    }
+
+    public void setModels(List<GrobidConfig.ModelParameters> models) {
+        this.models = models;
     }
 }
